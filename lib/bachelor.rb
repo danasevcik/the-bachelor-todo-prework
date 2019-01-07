70d230eedaa68d2c637682835d113f53a6df19e7
github username <github email address>
2019-01-06 19:03:31 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,66 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |szn, array|
+    if szn == season
+      array.each do |person|
+        person.each do |attribute, value|
+          if person["status"] = "Winner"
+            win_name = person["name"].split(" ")[0]
+            return win_name
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |szn, array|
+    array.each do |person|
+      person.each do |attribute, value|
+        if person["occupation"] == occupation
+          ans_name = person["name"]
+          return ans_name
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |szn, array|
+    array.each do |person|
+      if person["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |szn, array|
+    array.each do |person|
+      if person["hometown"] == hometown
+        ans_occ = person["occupation"]
+        return ans_occ
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  count_num = 0
+  count_age = 0
+  data.each do |szn, array|
+    if szn == season
+      array.each do |person|
+        count_num += 1
+        count_age += person["age"].to_i
+      end
+    end
+  end
+  avg = count_age / count_num.to_f
+  return avg.round
+
 end
--- spec/bachelor_spec.rb
+++ spec/bachelor_spec.rb
@@ -1,11 +1,11 @@
-describe "bachelor" do 
+describe "bachelor" do
 
   let(:data) { JSON.parse(File.read('spec/fixtures/contestants.json')) }
 
   describe "#get_first_name_of_season_winner" do
     it "returns 'Tessa' when passed the data and 'season 10'" do
       expect(get_first_name_of_season_winner(data, "season 10")).to eq("Tessa")
-    end
+    end 
 
     it "returns 'Courtney' when passed the data and 'season 16'" do
       expect(get_first_name_of_season_winner(data, "season 16")).to eq("Courtney")
@@ -71,4 +71,3 @@ describe "bachelor" do
   end
 
 end
-
